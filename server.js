ae466101d66ca8fb83472beca0ea1dd5205f24ff
Trang Do <quynhtrangdo94@gmail.com>
2019-06-21 13:27:44 -0400

    Routed files with express router

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB();
 
 app.get('/', (req,res) => res.send('API running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
