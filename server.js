5a031a0bc1e50bcccef2e7adc2efa253e9f5e54f
Trang Do <quynhtrangdo94@gmail.com>
2019-06-21 10:56:39 -0400

    Spinned up server and connected to DB

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Connect to database
+connectDB();
 
 app.get('/', (req,res) => res.send('API running'));
 
