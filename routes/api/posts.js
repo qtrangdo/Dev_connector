f0084b993fbb8ed3a09ace0cf8c81ccf2a3b8ca0
Trang Do <quynhtrangdo94@gmail.com>
2019-06-24 15:35:57 -0400

    Handled get and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -22,7 +22,7 @@ router.post('/', [
   try {
     const user = await User.findById(req.user.id).select('-password');
 
-    const newPost = new Post ({
+    const newPost = new Post({
       text: req.body.text,
       name: user.name,
       avatar: user.avatar,
@@ -33,6 +33,64 @@ router.post('/', [
 
   } catch (err) {
     console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
+
+// @route   GET api/posts/:post_id
+// @desc    Get post by id
+// @access  Private
+router.get('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+})
+
+// @route   DELETE api/posts/:post_id
+// @desc    Get post by id
+// @access  Private
+router.delete('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    // Check User, only post owner can delete the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+    await post.remove();
+    res.json({ msg: 'Post removed' });
+
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
     res.status(500).send('Server Error');
   }
 })
