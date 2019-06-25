085a2c107c84d82aef82bc24d830969f9adff9bd
Trang Do <quynhtrangdo94@gmail.com>
2019-06-24 23:43:25 -0400

    Created front end with create-react-app, changed server port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Connect to database
