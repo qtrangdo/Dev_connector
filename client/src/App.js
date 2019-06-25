5e08691240f68a6403d751842552124c28da9363
Trang Do <quynhtrangdo94@gmail.com>
2019-06-25 14:18:07 -0400

    Created Alert component, handled remove alert action

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 
 import NavBar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
@@ -18,6 +19,7 @@ const App = () => (
         <NavBar />,
       <Route exact path='/' component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
