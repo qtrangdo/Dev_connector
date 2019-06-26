9037398aecdea11a3e302f2b74340dbf47d2a123
Trang Do <quynhtrangdo94@gmail.com>
2019-06-26 17:02:30 -0400

    Rendered user education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
+import Education from './Education';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 
@@ -26,6 +27,7 @@ const Dashboard = ({ getCurrentProfile, user, profile, loading }) => {
           <Fragment>
             <DashboardActions />
             <Experience experience={profile.experience}/>
+            <Education education={profile.education}/>
           </Fragment>
           :
           <Fragment>
