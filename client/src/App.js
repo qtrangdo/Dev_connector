e032559d16997eca1aabb2f4acd48f8f081728bc
Trang Do <quynhtrangdo94@gmail.com>
2019-06-26 11:59:16 -0400

    Added create-profile form

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
@@ -38,6 +39,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/creat-profile' component={CreateProfile}/>
             </Switch>
           </section>
         </Fragment>
