a265d11dd76cfe1bac350ecc4bb3246619a42728
Trang Do <quynhtrangdo94@gmail.com>
2019-06-26 17:41:26 -0400

    Modified delete profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    GET current user profile
@@ -128,9 +129,10 @@ router.get('/user/:user_id', async (req, res) => {
 // @route   DELETE api/profile/
 // @desc    Delete profile, user, posts
 // @access  Private
-router.get('/', auth, async (req, res) => {
+router.delete('/', auth, async (req, res) => {
   try {
-    // @ todo - remove user & posts
+    // Remove User posts
+    await Post.deleteMany({ user: req.user.id })
     // Remove Profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
