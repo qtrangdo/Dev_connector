a1c3d55924e18aedcc0255010cf337f77f0de790
Trang Do <quynhtrangdo94@gmail.com>
2019-06-26 09:10:17 -0400

    Created Dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
@@ -35,6 +36,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
