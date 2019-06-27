78c9eb351187061d17287c11f13393dfd50ac9af
Trang Do <quynhtrangdo94@gmail.com>
2019-06-27 12:20:46 -0400

    Rendered profile item

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const ProfileItem = ({ profile }) => {
+  const { user, status, company, location, skills } = profile;
+  const { _id, name, avatar } = user;
+
   return (
-    <div>
-      Test
+    <div className="profile bg-light">
+      <img src={avatar} alt="" className="round-img" />
+      <div>
+        <h2>{name}</h2>
+        <p>{status} {company && <span> at {company}</span>}</p>
+        <p className="my-1">{location && <span>{location}</span>}</p>
+        <Link to={`/profile/${_id}`} className="btn btn-primary">View Profile</Link>
+      </div>
+      <ul>
+        {skills.slice(0,4).map((skill, index) => (
+          <li key={index}className="text-primary">
+            <i className="fas-fa-check"></i> {skill}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 ProfileItem.propTypes = {
-
+  profile: PropTypes.object.isRequired,
 }
 
 export default ProfileItem
