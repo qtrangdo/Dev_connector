e3ed7b152f094e102d5c8b3499d5db9d39c9fbcd
Trang Do <quynhtrangdo94@gmail.com>
2019-06-28 11:35:05 -0400

    Displayed post item

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,15 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, posts, loading }) => {
   useEffect(() => {
     getPosts()
   }, [getPosts])
   return (
-    <div>
-
-    </div>
+    loading ? < Spinner /> : (
+      <Fragment>
+        <h1 className="large text-primary">Posts</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Welcome to the community
+        </p>
+        {/* TODO: POST FORM */}
+        <div className="posts">
+          {posts.map(post => (
+            <PostItem key={post._id} post={post} />
+          ))}
+        </div>
+      </Fragment>
+    )
   )
 }
 
@@ -21,7 +33,7 @@ Posts.propTypes = {
   loading: PropTypes.bool.isRequired,
 }
 const mapStateToProps = state => ({
-  posts: state.post,
+  posts: state.post.posts,
   loading: state.post.loading
 })
 
