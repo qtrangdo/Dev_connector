3b1d96aa1f308611061351de61bc2d4a567d359d
Trang Do <quynhtrangdo94@gmail.com>
2019-06-28 13:44:43 -0400

    Implemented adding post and link to user

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,6 +5,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from './types';
 
 // Get all posts
@@ -66,6 +67,28 @@ export const deletePost = post_id => async dispatch => {
       payload: post_id
     });
     dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Add Post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  try {
+    const res = await axios.post('/api/posts', formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+    dispatch(setAlert('Post Created', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,8 @@ import {
   POST_ERROR,
   GET_POSTS,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from '../actions/types';
 
 const initialState = {
@@ -18,6 +19,12 @@ export default function (state = initialState, { type, payload }) {
       return {
         ...state,
         posts: payload,
+        loading: false
+      }
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
         loading: false
       }
     case DELETE_POST:
